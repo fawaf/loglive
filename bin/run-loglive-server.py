c53d45bb3834ad2b3413a63e00ab9e217bb873a1
Kenny Do <kennydo@berkeley.edu>
2013-07-13 11:13:06 -0700

    having httpserver listen to the port instead of the application, in preparation for using ssl

--- bin/run-loglive-server.py
+++ bin/run-loglive-server.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from loglive import config
 from loglive.web import application
+from tornado.httpserver import HTTPServer
 import tornado.ioloop
 
 
 if __name__ == "__main__":
-    application.listen(config.TORNADO_PORT)
+    server = HTTPServer(application)
+    server.listen(config.TORNADO_PORT)
     tornado.ioloop.IOLoop.instance().start()
