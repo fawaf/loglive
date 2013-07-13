9c9e7df488393d1db516303f966e9136c61606b1
Kenny Do <kennydo@berkeley.edu>
2013-07-13 04:54:19 -0700

    created setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="loglive",
+    version="0.0.1",
+    description="Live log-viewer powered by Tornado, ZeroMQ, and websockets",
+    author="Kenny Do",
+    author_email="kedo@ocf.berkeley.edu",
+    license="MIT License",
+    packages=["loglive"],
+    install_requires=[
+        "pyinotify>=0.9.4",
+        "pyzmq>=13.1.0",
+        "tornado>=0.1.2",
+    ]
+    )
