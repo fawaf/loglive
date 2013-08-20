91c6377b422ac9a9be3f58d9fc71224abded06b4
Kenny Do <kedo@ocf.berkeley.edu>
2013-08-20 06:31:12 -0700

    got the Network service to query successfully

--- loglive/static/js/services.js
+++ loglive/static/js/services.js
@@ -2,6 +2,13 @@
 
 angular.module('logliveServices', ['ngResource']).
 factory('Network', function($resource){
-    return $resource('/networks/:networkName.json', {networkName: '@id'}, {
+    return $resource('/api/networks/:networkName.json', {networkName: '@id'}, {
+        query: {
+            method: 'GET',
+            isArray: true,
+            transformResponse: function($data, $headersGetter){
+                return angular.fromJson($data)['networks'];
+            }
+        }
     });
 });
