290cabb44001c5baceaa830545ffbf7824c0f6f2
Kenny Do <kedo@ocf.berkeley.edu>
2013-08-20 20:11:12 -0700

    created get_{network,channel}_or_404 helper functions

--- loglive/handlers/api.py
+++ loglive/handlers/api.py
@@ -6,7 +6,31 @@ from loglive.models import IrcNetwork
 
 class LogLiveRequestHandler(RequestHandler):
     def get_user(self):
+        """
+        Returns the identifier for the current user, which is used
+        in the access control lists
+        """
         return self.get_secure_cookie('email')
+
+    def get_network_or_404(self, network_name):
+        """
+        Returns an IrcNetwork for the given network name, or raises 404
+        """
+        try:
+            network = IrcNetwork.get(network_name)
+        except ValueError:
+            self.send_error(status_code=404)
+        return network
+
+    def get_channel_or_404(self, network, channel_name):
+        """
+        Returns an IrcChannel for the given IrcNetwork and channel name,
+        or raises 404
+        """
+        channel = network.get_channel(channel_name)
+        if not channel:
+            self.send_error(status_code=404)
+        return channel
 
 
 class NetworkListHandler(LogLiveRequestHandler):
@@ -20,10 +44,7 @@ class NetworkListHandler(LogLiveRequestHandler):
 
 class NetworkDetailHandler(LogLiveRequestHandler):
     def get(self, network_name):
-        try:
-            network = IrcNetwork.get(network_name)
-        except ValueError:
-            self.send_error(status_code=404)
+        network = self.get_network_or_404(network_name)
         user = self.get_user()
         channel_names = sorted(channel.name for channel
                                in network.get_channels()
@@ -39,14 +60,8 @@ class NetworkDetailHandler(LogLiveRequestHandler):
 
 class ChannelDetailHandler(LogLiveRequestHandler):
     def get(self, network_name, channel_name):
-        try:
-            network = IrcNetwork.get(network_name)
-        except ValueError:
-            self.send_error(status_code=404)
-        channel = network.get_channel(channel_name)
-        if not channel:
-            self.send_error(status_code=404)
-
+        network = self.get_network_or_404(network_name)
+        channel = self.get_channel_or_404(network, channel_name)
         user = self.get_user()
         if not user_can_access_channel(user, network_name, channel_name):
             self.send_error(status_code=403)
@@ -60,15 +75,10 @@ class ChannelDetailHandler(LogLiveRequestHandler):
 
 class LogDetailHandler(LogLiveRequestHandler):
     def get(self, network_name, channel_name, date_string):
-        try:
-            network = IrcNetwork.get(network_name)
-        except ValueError:
-            self.send_error(status_code=404)
-        channel = network.get_channel(channel_name)
-        if not channel:
-            self.send_error(status_code=404)
-
+        network = self.get_network_or_404(network_name)
+        channel = self.get_channel_or_404(network, channel_name)
         user = self.get_user()
+
         if not user_can_access_channel(user, network_name, channel_name):
             self.send_error(status_code=403)
 
@@ -85,6 +95,7 @@ class LogDetailHandler(LogLiveRequestHandler):
                'network': network_name,
                'lines': lines}
         self.write(ret)
+
 
 handlers = [
             URLSpec(r'/api/networks\.json',
